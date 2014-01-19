40b18d58652c220a4f86f3fd790ee490121be926
C J Silverio <ceejceej@gmail.com>
2014-01-18 21:48:01 -0800

    Watch Less files with gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var
 	gulp       = require('gulp'),
 	browserify = require('gulp-browserify'),
+	changed    = require('gulp-changed'),
 	gutil      = require('gulp-util'),
 	less       = require('gulp-less'),
 	watch      = require('gulp-watch'),
@@ -15,7 +16,7 @@ gulp.task('default', function()
 gulp.task('css', function()
 {
 	gulp.src('./build/less/**/*.less')
-	.pipe(watch())
+	.pipe(changed('./site/public/css'))
 	.pipe(less({
 		paths: [ path.join(__dirname, 'bower_components', 'bootstrap', 'less') ]
 	}))
@@ -35,4 +36,14 @@ gulp.task('libs', function()
 gulp.task('js', function()
 {
 	// browserify it up
+});
+
+gulp.task('watch-css', function()
+{
+	gulp.src('./build/less/**/*.less')
+	.pipe(watch())
+	.pipe(less({
+		paths: [ path.join(__dirname, 'bower_components', 'bootstrap', 'less') ]
+	}))
+	.pipe(gulp.dest('./site/public/css'));
 });
